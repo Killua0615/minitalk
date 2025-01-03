ed10bdd7b0370f612b3de9fe4cf40aaf6adac2dc
Killua <106412328+Killua0615@users.noreply.github.com>
2025-01-04 05:29:29 +0900

    fixed

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: natsumi <natsumi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 21:33:49 by natsumi           #+#    #+#             */
-/*   Updated: 2025/01/04 05:24:18 by natsumi          ###   ########.fr       */
+/*   Updated: 2025/01/04 05:28:13 by natsumi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,11 +44,8 @@ static char	*append_char(const char *str, char c)
 	return (new_str);
 }
 
-static void	process_signal(int sig, int *bit_count, int *ascii_val,
-		char **message)
+static void	process_bit(int sig, int *bit_count, int *ascii_val, char **message)
 {
-	unsigned char byte;
-
 	if (!*message)
 	{
 		*message = ft_strdup("");
@@ -58,21 +55,6 @@ static void	process_signal(int sig, int *bit_count, int *ascii_val,
 	if (sig == SIGUSR2)
 		*ascii_val += calc_power(7 - *bit_count);
 	(*bit_count)++;
-	if (*bit_count == 8)
-	{
-		byte = (unsigned char)*ascii_val;
-		*message = append_char(*message, byte);
-		if (!*message)
-			return;
-		if (byte == '\0')
-		{
-			ft_printf("%s\n", *message);
-			free(*message);
-			*message = NULL;
-		}
-		*bit_count = 0;
-		*ascii_val = 0;
-	}
 }
 
 static void	handle_signal(int sig)
@@ -81,31 +63,21 @@ static void	handle_signal(int sig)
 	static int	ascii_val;
 	static char	*message;
 
-	if (!message)
+	process_bit(sig, &bit_count, &ascii_val, &message);
+	if (bit_count == 8)
 	{
+		message = append_char(message, (unsigned char)ascii_val);
+		if (!message)
+			return;
+		if ((unsigned char)ascii_val == '\0')
+		{
+			ft_printf("%s\n", message);
+			free(message);
+			message = NULL;
+		}
 		bit_count = 0;
 		ascii_val = 0;
-		message = NULL;
 	}
-	process_signal(sig, &bit_count, &ascii_val, &message);
-}
-
-static int setup_signal_handlers(struct sigaction *sa)
-{
-	sa->sa_handler = handle_signal;
-	sa->sa_flags = 0;
-	sigemptyset(&sa->sa_mask);
-	if (sigaction(SIGUSR1, sa, NULL) == -1)
-	{
-		ft_printf("Error: Failed to set SIGUSR1 handler\n");
-		return (0);
-	}
-	if (sigaction(SIGUSR2, sa, NULL) == -1)
-	{
-		ft_printf("Error: Failed to set SIGUSR2 handler\n");
-		return (0);
-	}
-	return (1);
 }
 
 int	main(void)
@@ -113,8 +85,15 @@ int	main(void)
 	struct sigaction sa;
 
 	ft_printf("Server PID: %d\n", getpid());
-	if (!setup_signal_handlers(&sa))
+	sa.sa_handler = handle_signal;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || 
+		sigaction(SIGUSR2, &sa, NULL) == -1)
+	{
+		ft_printf("Error: Failed to set signal handlers\n");
 		return (1);
+	}
 	while (1)
 		pause();
 	return (0);
